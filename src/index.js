0a90796dc9fe5d858065285f778278fa6f4db57d
Dmitrii Barilkin <dimabaril@gmail.com>
2023-08-02 04:50:50 +0300

    easyyy )

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = "";
+    let absStr = Math.abs(n).toString();
+
+    let i = 0;
+    while (i < absStr.length) {
+        result = absStr[i] + result;
+        i++;
+    }
+    return Number(result);
+};
